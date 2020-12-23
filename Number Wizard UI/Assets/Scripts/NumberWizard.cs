9db9ba4e4dc60bf8d0b93cbb120510875c4b389e
Siddharth Singhai <sidsinghai97@gmail.com>
2020-12-23 19:28:10 +0530

    Initial commit
    Uploading the code base to  repo

---
+++ Number Wizard UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]int max;
+    [SerializeField]int min;
+    [SerializeField] TextMeshProUGUI guessText;
+    int guess;
+    void Start()
+    {
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        NextGuess();
+        max = max + 1;
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess+1;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess-1;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = Random.Range(min,max);
+        guessText.text = guess.ToString();
+    }
+}
